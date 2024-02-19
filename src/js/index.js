e1853543bca0626bc08a76416df30b33d493af41
MarekLempart <kama.zakupy@gmail.com>
2024-02-19 20:53:44 +0100

    Dalsza praca nad zadaniem hw11

--- src/js/index.js
+++ src/js/index.js
@@ -33,6 +33,51 @@ async function searchImages(query, page = 1) {
   }
 }
 
+// images.hits.map(
+//   ({
+//     webformatURL,
+//     largeImageURL,
+//     tags,
+//     likes,
+//     views,
+//     comments,
+//     downloads,
+//   }) => `
+// <div class="photo-card">
+//   <a href="${largeImageURL}">
+//     <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//   </a>
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//       <br>${likes}
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//       <br>${views}
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//       <br>${comments}
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//       <br>${downloads}
+//     </p>
+//   </div>
+// </div>`
+// );
+
+// od Marcina
+// const queryString = "yellow flowers";
+// const preparedQueryString = queryString.split(" ").join("+");
+// fetch(
+//   `https://pixabay.com/api/?key=42451517-7ac5a5d17c420ae469b144174&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=1`
+// )
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+//koniec
+
 // Funkcja renderująca pojedynczą kartę obrazka
 function renderImageCard(image) {
   const card = document.createElement('div');
